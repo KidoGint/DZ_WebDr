5f3c80ab55df42acf9e195dd443bc4d0d916dac6
KIdoGint <kido2201@gmail.com>
2021-06-25 17:59:53 +0300

    3.2.8

--- src/test/java/StepDef/Hook.java
+++ src/test/java/StepDef/Hook.java
@@ -1,16 +1,17 @@
 package StepDef;
 
-import org.junit.After;
-import org.junit.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class hook {
+public class Hook {
     public static WebDriver driver;
     @Before
     public void before(){
         System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
     @After
     public void after(){
--- src/test/java/StepDef/StepDef.java
+++ src/test/java/StepDef/StepDef.java
@@ -14,8 +14,7 @@ public class StepDef {
 
     @Пусть("открыт ресурс авито")
     public static void openAvito() {
-        hook.driver.manage().window().maximize();
-        hook.driver.get("https://www.avito.ru/");
+        Hook.driver.get("https://www.avito.ru/");
     }
 
     @ParameterType(".*")
@@ -26,45 +25,45 @@ public class StepDef {
     @И("в выпадающем списке категорий выбрана {categories}")
     public static void chooseCategory(Categories category) {
 
-        WebElement params = hook.driver.findElement(By.xpath("//option[@value =" + category.getValue() + "]"));
+        WebElement params = Hook.driver.findElement(By.xpath("//option[@value =" + category.getValue() + "]"));
         params.click();
     }
 
     @И("в поле поиска введено значение {word}")
     public void printItem(String word) {
-        hook.driver.findElement(By.xpath("//input[@data-marker='search-form/suggest']")).sendKeys(word);
+        Hook.driver.findElement(By.xpath("//input[@data-marker='search-form/suggest']")).sendKeys(word);
     }
 
     @Тогда("кликнуть по выпадающему списку региона")
     public void ClickRegion() {
-        hook.driver.findElement(By.xpath("//div[@data-marker='search-form/region']")).click();
+        Hook.driver.findElement(By.xpath("//div[@data-marker='search-form/region']")).click();
     }
 
     @Тогда("в поле регион введено значение {word}")
     public void printRegion(String word) throws Exception {
-        hook.driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']")).click();
-        hook.driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']")).sendKeys(word);
+        Hook.driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']")).click();
+        Hook.driver.findElement(By.xpath("//input[@data-marker='popup-location/region/input']")).sendKeys(word);
         Thread.sleep(500);
-        hook.driver.findElement(By.xpath("//li[@data-marker='suggest(0)']")).click();
+        Hook.driver.findElement(By.xpath("//li[@data-marker='suggest(0)']")).click();
     }
 
     @И("нажата кнопка показать объявления")
     public void clickShow() throws Exception {
-        hook.driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']")).click();
+        Hook.driver.findElement(By.xpath("//button[@data-marker='popup-location/save-button']")).click();
         Thread.sleep(500);
     }
 
     @Тогда("открылась страница результата по запросу {word}")
     public void checkPage(String word) {
-        if (hook.driver.findElement(By.xpath("//h1[contains(@class, 'page-title')]")).getText().contains(word))
+        if (Hook.driver.findElement(By.xpath("//h1[contains(@class, 'page-title')]")).getText().contains(word))
             System.out.println("Страница открылась");
     }
 
     @И("активирован чекбокс С Авито Доставкой")
     public void checkboxActivate() {
-        if (!hook.driver.findElement(By.xpath("//label[@data-marker='delivery-filter']")).getAttribute("class").contains("checked"))
-            hook.driver.findElement(By.xpath("//label[@data-marker='delivery-filter']")).click();
-        hook.driver.findElement(By.xpath("//button[@data-marker='search-filters/submit-button']")).click();
+        if (!Hook.driver.findElement(By.xpath("//label[@data-marker='delivery-filter']")).getAttribute("class").contains("checked"))
+            Hook.driver.findElement(By.xpath("//label[@data-marker='delivery-filter']")).click();
+        Hook.driver.findElement(By.xpath("//button[@data-marker='search-filters/submit-button']")).click();
     }
 
     @ParameterType(".*")
@@ -74,18 +73,17 @@ public class StepDef {
 
     @И("в выпадающей списке сортировка выбрано значение {sorts}")
     public void sortByHighPrice(Sorts sort) {
-        hook.driver.findElement(By.xpath("//div[contains(@class,'sort')]//option[@data-marker='option(" + sort.getValue() + ")']")).click();
+        Hook.driver.findElement(By.xpath("//div[contains(@class,'sort')]//option[@data-marker='option(" + sort.getValue() + ")']")).click();
     }
 
     @И("в консоль выведено значение назвния и цены {int} первых товаров")
     public void printTop(int N) {
-        List<WebElement> list = hook.driver.findElements(By.xpath("//div[@data-marker='item']"));
+        List<WebElement> list = Hook.driver.findElements(By.xpath("//div[@data-marker='item']"));
         for (int i = 0; i < N; i++) {
             String name = list.get(i).findElement(By.xpath(".//h3[@itemprop='name']")).getText();
             String price = list.get(i).findElement(By.xpath(".//span[contains(@class,'price-text')]")).getText();
             System.out.println(name + " " + price);
         }
-        hook.driver.close();
 
     }
 }
